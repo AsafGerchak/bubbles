cd3caffe809873ab2191fea2363dd697fcfa21be
AsafGerchak <asaf.gerchak@gmail.com>
2015-01-15 22:29:04 -0500

    first push of early site files! :rocket:

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Doc ready!
+$(function(){
+
+	$('.nav').on('click', function(){
+		$('#dropper1').toggleClass('close1').toggleClass('open1');
+		$('#dropper2').toggleClass('open2');
+		$('#dropper3').toggleClass('open3');
+		$('#dropper4').toggleClass('open4');
+	});
+
+});
